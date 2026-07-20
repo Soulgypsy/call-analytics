9539ad2e93267f3f0145a049744dd9a231ea5f7a
Soulgypsy <72500104+Soulgypsy@users.noreply.github.com>
2026-07-20 12:25:56 +0100

    deleting mystery duplicate import

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
